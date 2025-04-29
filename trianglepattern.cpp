cff19efb303b26c3d00f0cc714b15e3800761028
Nathanael Zamora Satria Nugroho <672023013@student.uksw.edu>
2025-04-29 12:56:37 +0700

    Refactor triangle pattern functions for improved structure and readability

--- trianglepattern.cpp
+++ trianglepattern.cpp
@@ -10,10 +10,34 @@ void rightAngledTriangle(int n) {
     }
 }
 
+void leftAngledTriangle(int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++) {
+            cout << "  "; // Print spaces
+        }
+        for (int j = 1; j <= i; j++) {
+            cout << "* "; // Print stars
+        }
+        cout << endl;
+    }
+}
+
 void invertedRightAngledTriangle(int n) {
     for (int i = n; i >= 1; i--) {
         for (int j = 1; j <= i; j++) {
             cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+void invertedLeftAngledTriangle(int n) {
+    for (int i = n; i >= 1; i--) {
+        for (int j = 1; j <= n - i; j++) {
+            cout << "  "; // Print spaces
+        }
+        for (int j = 1; j <= i; j++) {
+            cout << "* "; // Print stars
         }
         cout << endl;
     }
@@ -43,39 +67,6 @@ void invertedEquilateralTriangle(int n) {
     }
 }
 
-void diamondPattern(int n) {
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n - i; j++) {
-            cout << "  ";
-        }
-        for (int j = 1; j <= 2 * i - 1; j++) {
-            cout << "* ";
-        }
-        cout << endl;
-    }
-    for (int i = n - 1; i >= 1; i--) {
-        for (int j = 1; j <= n - i; j++) {
-            cout << "  ";
-        }
-        for (int j = 1; j <= 2 * i - 1; j++) {
-            cout << "* ";
-        }
-        cout << endl;
-    }
-}
-
-void hollowSquare(int n) {
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            if (i == 1 || i == n || j == 1 || j == n) {
-                cout << "* ";
-            } else {
-                cout << "  ";
-            }
-        }
-        cout << endl;
-    }
-}
 
 int main() {
     int n;
@@ -85,19 +76,20 @@ int main() {
     cout << "\nRight Angled Triangle:\n";
     rightAngledTriangle(n);
 
+    cout << "\nLeft Angled Triangle:\n";
+    leftAngledTriangle(n);
+
     cout << "\nInverted Right Angled Triangle:\n";
     invertedRightAngledTriangle(n);
 
+    cout << "\nInverted Left Angled Triangle:\n";
+    invertedLeftAngledTriangle(n);
+    
     cout << "\nEquilateral Triangle:\n";
     equilateralTriangle(n);
 
     cout << "\nInverted Equilateral Triangle:\n";
     invertedEquilateralTriangle(n);
 
-    cout << "\nDiamond Pattern:\n";
-    diamondPattern(n);
-
-    cout << "\nHollow Square:\n";
-    hollowSquare(n);
     return 0;
 }
